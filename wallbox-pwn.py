a8da660abbe36407c26e1cd6f2bfc2f2e7e66e84
Fredrik Gustafsson <jagheterfredrik@gmail.com>
2023-08-17 12:53:57 +0200

    add pairing to simplify for windows
    OS X pairs automatically, Windows doesn’t

--- wallbox-pwn.py
+++ wallbox-pwn.py
@@ -21,6 +21,7 @@ class WallboxBLE():
     async def connect(self, device):
         self.client = BleakClient(device, timeout=30)
         await self.client.connect()
+        await self.client.pair()
 
         await self.client.start_notify(UART_TX_CHAR_UUID, self.handle_rx)
         nus = self.client.services.get_service(UART_SERVICE_UUID)
